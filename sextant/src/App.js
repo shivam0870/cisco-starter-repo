f34fef9163b374d1cf585a438238927f4c84aa23
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-07-09 17:08:12 +0530

    All Changes Done

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import './index.css'
 import Banner from "./components/Banner/Banner";
 import Exhibit from "./components/Exhibit/Exhibit";
 import Address from "./components/Address/Address";
+import Connect  from './components/Connect/PylonConnect';
 
 const App = () => {
   return (
-    <div>
-      <Banner />
-       
+    <div className="App">
+                <Banner bannerText="Sextant" />
                 <Exhibit name="Public IPv4 Address">
                     <Address url='https://api.ipify.org?format=json' />
                 </Exhibit>
                 <Exhibit name="Public IPv6 Address">
                     <Address url='https://api64.ipify.org?format=json' />
                 </Exhibit>
-    </div>
+                <Exhibit name="Pylon Packet Latency">
+                    <Connect />
+                </Exhibit>
+            </div>
   );
 };
 
