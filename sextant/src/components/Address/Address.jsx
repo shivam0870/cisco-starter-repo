9484d19fecdec533fe30e4251cbed45ca6a34f3b
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2023-07-09 16:44:37 +0530

    Address

--- sextant/src/components/Address/Address.jsx
+++ sextant/src/components/Address/Address.jsx
@@ -1,31 +1,3 @@
-// import React, { Component } from 'react';
-
-// class AddressDisplay extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             url: props.url,
-//             ipAddress: null
-//         };
-//     }
-
-//     componentDidMount() {
-//         fetch(this.state.url)
-//             .then(response => response.json())
-//             .then(data => this.setState({ ipAddress: data.ip }));
-//     }
-
-//     render() {
-//         return (
-//             <span className="AddressDisplay">
-//                 {this.state.ipAddress}
-//             </span>
-//         );
-//     }
-// }
-
-// export default AddressDisplay;
-
 
 
 import React, { useEffect, useState } from 'react';
